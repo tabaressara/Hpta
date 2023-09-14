55bbf5c3e6720817830f6827a64f9db5be80880d
tabaressara <tabaressara1110@gmail.com>
2023-09-14 09:40:16 -0500

    no voy a hacer más

--- src/pages/Partidos/Partidos.js
+++ src/pages/Partidos/Partidos.js
@@ -1,12 +1,32 @@
 
 import Navigator from "../../components/NavigatorBar/Navigator";
+import styles from "./Partidos.module.css"
+import Contacto from "../../components/barraBaja/barraBaja";
+import Login from "../../components/Login/LoginModal";
 
 function Partidos() {
     return (
-        <div>
+        <div className = {styles.fondo}>
+            <Login/>
             <Navigator/>
+            <div className = {styles.base}>
+                <img src="imagen6.jpg" className = {styles.imagen} alt="Imagen" />
+                <div>
+                    <h1 className = {styles.titulo}>
+                        A jugar!
+                    </h1>
+                    <p className = {styles.texto}>
+                        ¡Prepárate para jugar y <br/>
+                        disfrutar al máximo en la cancha!</p>
+                    <button className = {styles.boton}>
+                        ver reglamento
+                    </button>
+                </div>
+            </div>
+            <Contacto/>
         </div>
     );
 }
+
 
 export default Partidos;
--- src/pages/Recomendaciones/Recomendaciones.js
+++ src/pages/Recomendaciones/Recomendaciones.js
@@ -1,10 +1,30 @@
 
 import Navigator from "../../components/NavigatorBar/Navigator";
+import styles from "./Recomendaciones.module.css";
+import Contacto from "../../components/barraBaja/barraBaja";
+import Login from "../../components/Login/LoginModal";
 
 function Recomendaciones() {
     return (
-        <div>
+        <div className = {styles.fondo}>
+            <Login/>
             <Navigator/>
+            <div className = {styles.base}>
+                <img src="imagen5.jpg" className = {styles.imagen} alt="Imagen" />
+                <div>
+                    <p className = {styles.texto}>
+                        Te invitamos a explorar nuestra sección de recomendaciones <br/>
+                        para jugar, donde encontrarás valiosos consejos y <br/>
+                        estrategias para mejorar tus habilidades en el voleibol.
+                        <br/> <br/> <br/>
+                        No importa si eres un principiante o un jugador experimentado, <br/>
+                        siempre hay espacio para aprender.</p>
+                    <button className = {styles.boton}>
+                        ver recomendaciones
+                    </button>
+                </div>
+            </div>
+            <Contacto/>
         </div>
     );
 }
