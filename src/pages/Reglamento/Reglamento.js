b7decc501d67688f24d9f34d38366eaeba078d94
tabaressara <tabaressara1110@gmail.com>
2023-09-15 08:45:35 -0500

    no se

--- src/pages/Reglamento/Reglamento.js
+++ src/pages/Reglamento/Reglamento.js
@@ -6,7 +6,7 @@ import Login from "../../components/Login/LoginModal";
 import Registrarse from "../../components/Registrarse/Registrarse";
 
 function Reglamento() {
-    return (
+    /*return (
         <div className = {styles.fondo}>
             <Login/>
             <Navigator/>
@@ -28,6 +28,32 @@ function Reglamento() {
                     </button>
                 </div>
             </div>
+            <Contacto/>
+        </div>
+    );*/
+
+    return (
+        <div className = {styles.fondo}>
+            <Login/>
+            <Navigator/>
+            <Registrarse/>
+            <div className = {styles.base}>
+                <img src="imagen4.jpg" className = {styles.imagen} alt="Imagen" /> 
+                <div>
+                    <h1 className = {styles.titulo}>
+                        Reglamento del <br/>
+                        voleibol
+                    </h1>
+                    <p className = {styles.texto}>
+                        Antes de dar el primer saque, es esencial conocer <br/> 
+                        las reglas básicas del voleibol. <br/><br/>
+                        En esta sección, te ofrecemos una introducción al reglamento básico del <br/> 
+                        voleibol, para que puedas disfrutar de cada set.</p>
+                    <a href="#reglamento" className={styles.boton}>
+                        ver reglamento
+                    </a>
+                </div>
+            </div>    
             <Contacto/>
         </div>
     );
